c42e063df003e820ef7ca82d888c8230ae239a15
Hy Huynh <giahy33@gmail.com>
2023-11-20 09:53:27 -0700

    unsubmodule rdev

---
+++ rdev/src/linux/mod.rs
@@ -0,0 +1,20 @@
+extern crate libc;
+extern crate x11;
+
+mod common;
+mod display;
+#[cfg(feature = "unstable_grab")]
+mod grab;
+mod keyboard;
+mod keycodes;
+mod listen;
+mod simulate;
+
+pub use crate::linux::display::display_size;
+#[cfg(feature = "unstable_grab")]
+pub use crate::linux::grab::grab;
+#[cfg(feature = "unstable_grab")]
+pub use crate::linux::grab::grab_t;
+pub use crate::linux::keyboard::Keyboard;
+pub use crate::linux::listen::listen;
+pub use crate::linux::simulate::simulate;
